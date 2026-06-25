24bdd3de7a9125e3621d847c62b09de12267d36b
ChrisF <chrisferbia@gmail.com>
2026-06-26 02:48:28 +0700

    add shadcn component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger
+} from "@/components/ui/navigation-menu";
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <h1 className="text-3xl font-bold underline">Hellow World!</h1>
-        <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
-      </main>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/home">Home</Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>About</NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/contact">Contact</Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
     </div>
   );
 }
